b136a4bce66d2ac977d13612aa0357bd4e9beca7
rinjugatla <rin.jugatla@gmail.com>
2020-09-12 14:19:37 +0900

    MOD初期化コードを追加

--- AnyListLogger/Class1.cs
+++ AnyListLogger/Class1.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
 
 namespace AnyListLogger
 {
-    public class Class1
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ExamplePlugin : BaseUnityPlugin
     {
+        public const string PluginGuid = "2AA56D88-A06D-4790-89E7-5192585E1A41";
+        public const string PluginName = "AnyListLogger";
+        public const string PluginVersion = "0.0.1";
+
+        void Awake()
+        {
+            UnityEngine.Debug.Log($"{PluginName} : {PluginVersion}");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
     }
 }
