9a4b328f45b4cc666bc63392a8c4034b41e36ca3
rinjugatla <rin.jugatla@gmail.com>
2020-11-27 22:00:35 +0900

    スキル関係のプロジェクトを追加

---
+++ SkillManager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillManager
+{
+    public class Class1
+    {
+    }
+}
