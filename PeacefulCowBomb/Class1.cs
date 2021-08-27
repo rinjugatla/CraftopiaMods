9fd9d200e21328eabe22226f14345c7bd3b78650
rinjugatla <rin.jugatla@gmail.com>
2021-08-27 17:53:35 +0900

    自爆牛のダメージを無効化するMODを追加

---
+++ PeacefulCowBomb/Class1.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Oc;
+using Oc.Em;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeacefulCowBomb
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ExamplePlugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.PeacefulCowBomb";
+        private const string PluginName = "PeacefulCowBomb";
+        private const string PluginVersion = "0.0.2";
+
+        void Awake()
+        {
+            var harmony = new Harmony(PluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    /// <summary>
+    /// 自爆牛
+    /// </summary>
+    [HarmonyPatch(typeof(OcEm_CowBomb))]
+    internal class OcEm_CowBomb_Hook
+    {
+        /// <summary>
+        /// 火を無効化
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPatch("makeActionTable"), HarmonyPrefix]
+        static void MakeActionTable(OcEm_CowBomb __instance)
+        {
+            Traverse.Create(__instance.Condition).Field("IgnoreFire").SetValue(true);
+        }
+
+        /// <summary>
+        /// 爆発ダメージ
+        /// </summary>
+        /// <param name="__result"></param>
+        /// <returns></returns>
+        [HarmonyPatch("deathExplosionDmg"), HarmonyPrefix]
+        static bool DeathExplosionDmg(ref float __result)
+        {
+            __result = 0;
+            return false;
+        }
+
+        /// <summary>
+        /// 爆発サイズ
+        /// </summary>
+        /// <param name="__result"></param>
+        /// <returns></returns>
+        [HarmonyPatch("deathExplosionScale"), HarmonyPrefix]
+        static bool DeathExplosionScale(ref float __result)
+        {
+            __result = 0;
+            return false;
+        }
+    }
+}
---
+++ PeacefulCowBomb/ModUpload/readme.txt
@@ -0,0 +1,24 @@
+概要
+　爆弾樽牛の炎上、爆発ダメージを無効化します。
+
+連絡先
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+バージョン
+　0.0.1 公開
+　0.0.2 マルチサーバでの動作を確認
+        不要な処理を削除
+
+
+Overview
+　Disables the flame and explosion damage of bomb barrel cattle.
+
+Contact
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+Version
+　0.0.1 released
+　0.0.2 Check multi-server operation
+        Remove unnecessary processes
