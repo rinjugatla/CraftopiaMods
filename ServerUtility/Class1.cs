fda1e48b0262f70b52ade9692bd40b11d4123ab1
rinjugatla <rin.jugatla@gmail.com>
2021-08-27 17:53:58 +0900

    サーバ設定ファイル、保存先を変更するMODを追加

---
+++ ServerUtility/Class1.cs
@@ -0,0 +1,104 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Oc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerUtility
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ServerUtilityPlugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.ServerUtility";
+        private const string PluginName = "ServerUtility";
+        private const string PluginVersion = "0.0.1";
+
+        private static ConfigFile MyConfig = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{PluginGuid}.cfg"), true);
+        private static ConfigEntry<string> FolderName;
+        private static ConfigEntry<string> ServerSettingFileName;
+
+        void Awake()
+        {
+            FolderName = MyConfig.Bind<string>("General", "SaveFolderName", "DedicatedServerSave", "Enter a save folder name.");
+            ServerSettingFileName = MyConfig.Bind<string>("General", "ServerSettingFileName", "ServerSetting.ini", "Enter a server setting file name.");
+            new Harmony(PluginGuid).PatchAll();
+        }
+
+        [HarmonyPatch(typeof(OcAllSceneShareSingleton), "SERVER_SETTING_PATH", MethodType.Getter)]
+        public class OcAllSceneShareSingleton_Hook
+        {
+            static void Postfix(ref string __result)
+            {
+                if (ServerSettingFileName == null || ServerSettingFileName.Value == "")
+                    return;
+
+                string name = ServerSettingFileName.Value;
+                if (!name.EndsWith(".ini"))
+                    name += ".ini";
+
+                string path = UnityEngine.Application.persistentDataPath + "/" + name;
+
+                UnityEngine.Debug.Log($"{PluginName} ServerSettingFileName: {path}");
+
+                __result = path;
+            }
+        }
+
+        [HarmonyPatch(typeof(OcSaveManager), "OnUnityAwake")]
+        public class OcSaveManager_Hook
+        {
+            /// <summary>
+            /// セーブデータの保存先を変更
+            /// </summary>
+            /// <param name="___FolderPath"></param>
+            static void Postfix(OcSaveManager __instance,
+                ref string ___FolderPath, ref string ___BackupMinPath, ref string ___BackupHourPath, ref ES3Settings ____saveSetting)
+            {
+                // 不正な設定値の場合は初期値を使用
+                if (FolderName == null || FolderName.Value == "")
+                    return;
+
+                ___FolderPath = FixFolderName(FolderName.Value) + "/";
+
+                ___BackupMinPath = ___FolderPath + "SaveBackup_m";
+                ___BackupHourPath = ___FolderPath + "SaveBackup_h";
+                ____saveSetting = new ES3Settings(___FolderPath + "SaveData.ocs", settings: null);
+
+                UnityEngine.Debug.Log($"{PluginName} SaveFolderPath: {____saveSetting.FullPath}");
+
+                if (ES3.FileExists(____saveSetting))
+                    Traverse.Create(__instance).Method("TryCreateCache").GetValue();
+            }
+
+            /// <summary>
+            /// 不正なフォルダ名を補正
+            /// </summary>
+            /// <param name="name"></param>
+            /// <returns></returns>
+            private static string FixFolderName(string name)
+            {
+                string result = name;
+                // フルパスの場合は最後のフォルダ名だけを取得
+                if (result.IndexOf("://") > -1)
+                    result = Path.GetFileName(Path.GetDirectoryName(result));
+
+                // 不正な文字を削除
+                string[] invalidChars = new string[] { ":", "*", "?", "\"", "<", ">", "|" };
+                foreach (var s in invalidChars)
+                {
+                    result = result.Replace(s, "");
+                }
+
+                if (name != result)
+                    UnityEngine.Debug.Log($"{PluginName} FixFolderName: {name} -> {result}");
+
+                return result;
+            }
+        }
+    }
+}
---
+++ ServerUtility/ModUpload/readme.txt
@@ -0,0 +1,29 @@
+概要
+　セーブデータフォルダ名、サーバ設定ファイル名を変更します。
+
+　初回起動時に「BepInEx\config」フォルダ内に設定ファイル「me.rin_jugatla.craftopia.mod.ServerUtility.cfg」が自動作成されます。
+　設定は設定ファイルの説明を参照して下さい。
+　同梱の「sample - me.rin_jugatla.craftopia.mod.ServerUtility.cfg」は初期値の設定ファイルです。
+
+連絡先
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+バージョン
+　0.0.1 公開
+
+Summary
+　Changes the name of the saved data folder and server configuration file.
+
+　The configuration file "me.rin_jugatla.craftopia.mod.ServerUtility.cfg" will be automatically created in the "BepInEx\config" folder at the first startup.
+　Please refer to the description of the configuration file for the settings.
+　The included "sample - me.rin_jugatla.craftopia.mod.ServerUtility.cfg" is the default configuration file.
+
+Contact
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+Version
+　0.0.1 released
+
+Translated with www.DeepL.com/Translator (free version)
