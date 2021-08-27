e5e10c9e5b0a975b0caaf346e1df92800bfafa18
rinjugatla <rin.jugatla@gmail.com>
2021-08-27 17:54:22 +0900

    音の再生位置をカメラ位置からキャラクタの位置に変更するMODを追加

---
+++ FixEars/Class1.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Oc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FixEars
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ExamplePlugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "me.rin_jugatla.craftopia.mod.FixEars";
+        private const string PluginName = "FixEars";
+        private const string PluginVersion = "0.0.1";
+
+        private static ConfigFile MyConfig = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{PluginGuid}.cfg"), true);
+        private static ConfigEntry<bool> IsForwardByPlayer;
+
+        void Awake()
+        {
+            IsForwardByPlayer = MyConfig.Bind<bool>("General", "IsForwardByPlayer", false, "Either align the ears with the direction the character is facing, rather than the direction of the camera.");
+            new Harmony(PluginGuid).PatchAll();
+        }
+
+        [HarmonyPatch(typeof(OcAudioListenerCtrl))]
+        internal class OcAudioListenerCtrl_Hook
+        {
+            [HarmonyPatch("Update"), HarmonyPostfix]
+            static void OcAudioListenerCtrl_Update(Camera ___mainCamera, ref AudioListener ___audioListener)
+            {
+                if(IsForwardByPlayer.Value)
+                    ___audioListener.transform.forward = OcPlMaster.Inst.transform.forward;
+                ___audioListener.transform.position = OcPlMaster.Inst.transform.position;
+            }
+        }
+    }
+}
---
+++ FixEars/ModUpload/readme.txt
@@ -0,0 +1,35 @@
+概要
+　音の再生位置をカメラ位置からキャラクタの位置に変更します。
+　この変更により、キャラクタよりも後ろから音が聞こえる現象が改善されます。
+　プラグイン導入後初回のゲーム起動時に設定ファイルが作成されます。
+　設定ファイル名: me.rin_jugatla.craftopia.mod.FixEars.cfg
+
+設定
+　IsForwardByPlayer: デフォルトfalse
+　　true: キャラクタの方向と耳の方向が一致します。
+　　false: カメラの方向と耳の方向が一致します。(こちらの方が使いやすそう)
+
+連絡先
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+バージョン
+　0.0.1 公開
+
+Overview
+ Changes the sound playback position from the camera position to the character position.
+ This change will correct the problem of sound coming from behind the character.
+ A configuration file will be created the first time you run the game after installing the plugin.
+ Configuration file name: me.rin_jugatla.craftopia.mod.FixEars.cfg
+
+Settings
+ IsForwardByPlayer: default false
+  true: The direction of the character matches the direction of the ears.
+  false: The direction of the camera will match the direction of the ears. (This may be easier to use).
+
+Contact
+　Discord rin#5432
+　Twitter @rin_jugatla
+
+Version
+　0.0.1 released
